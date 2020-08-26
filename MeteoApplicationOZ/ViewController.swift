965f35618c2c88b4cee72c19285b628495bbdd3c
ZROUD Oussama <ousszroud@gmail.com>
2020-08-26 11:32:49 +0200

    Initial Commit

---
+++ MeteoApplicationOZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeteoApplicationOZ
+//
+//  Created by Oussama Zroud on 8/26/20.
+//  Copyright © 2020 Oussama Zroud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
